0b4aa3f1b226e48a98adc25f25945db1cf6f3ef9
RichLi <lichi0623@gmail.com>
2022-08-24 20:41:51 +0800

    sql update

--- server/__init__.py
+++ server/__init__.py
@@ -1,12 +1,16 @@
+import os
 from flask import Flask
 from server.extensions import socketio, cors, db
 from server.api.init_api import init_api
+
+MYSQL_USER = os.environ.get('MYSQL_USER')
+MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
 
 
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://<username>:<password>@<ip>/flask'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@db/flask'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     app.register_blueprint(init_api)
