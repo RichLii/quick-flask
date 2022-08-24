1d424930ea88feea5a8df6bce70952fa1a97f6eb
RichLi <lichi0623@gmail.com>
2022-08-24 22:15:19 +0800

    env update

--- runserver.sh
+++ runserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Mode debug or produce
 # !!! Mode produce can only run on Linux Base OS !!!
-MODE="${ENV_MODE:=debug}"
+MODE="${ENV_MODE}"
 export FLASK_DEBUG=true
 export FLASK_APP=server
 DIRNAME="$(dirname -- "$(readlink -f "${BASH_SOURCE}")")"
@@ -10,13 +10,16 @@ if [ -n "$MODE" ]
   then
   if [ $MODE = 'debug' ]
     then
+      echo 'debug server running...'
       flask run --host 0.0.0.0 --port=8000
     elif [ $MODE = 'produce' ]
     then
+      echo 'produce server running...'
       gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8000 "server.wsgi:app"
     else
     echo 'Mode must be debug or produce'
   fi
   else
-    echo 'Mode must not be null'
+    echo 'debug server running...'
+    flask run --host 0.0.0.0 --port=8000
 fi
