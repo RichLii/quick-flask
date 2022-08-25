4bfcb350d8829ca2f89613c144f9ee0f7f17c366
RichLi <lichi0623@gmail.com>
2022-08-25 22:03:00 +0800

    docker env update

--- docker/init.sh
+++ docker/init.sh
@@ -5,9 +5,14 @@ cd /home
 if [ ! -d /home/quick-flask ]
   then
   git clone https://github.com/RichLii/quick-flask.git \
-  && cd /home/quick-flask \
-  && pip install -r requirements.txt \
   && chmod +x /home/quick-flask/runserver.sh
 fi
 
-python
+if [ -f /home/quick-flask/requirements.txt ]
+  then
+  cd /home/quick-flask \
+  && pip install -r requirements.txt
+fi
+
+/home/quick-flask/runserver.sh \
+&& python
